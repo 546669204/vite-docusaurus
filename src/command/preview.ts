e4662b5936570c604c3b861ebb4f5507cb6de7a9
xiaoming <546669204@qq.com>
2023-02-19 11:46:37 +0800

    feat: preview support config

--- src/command/preview.ts
+++ src/command/preview.ts
@@ -1,14 +1,17 @@
 import { preview as vitePreview } from "vite";
 
-export default async function preview() {
+export default async function preview(option: Record<string, any>) {
   const app = await vitePreview({
-    root: "./build/",
+    root: option.outDir || "./build/",
     preview: {
-      port: 18018,
-      // open: true,
+      host: option.host,
+      port: option.port || 8080,
+      strictPort: option.strictPort,
+      https: option.https,
+      open: option.open,
     },
-    build:{
-      outDir:""
+    build: {
+      outDir: ""
     }
   });
   app.printUrls()
